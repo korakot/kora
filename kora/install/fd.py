e3c512582e9de35ed5062319964cf4d2bb2aac97
Korakot Chaovavanich <korakot@gmail.com>
2020-06-11 23:50:14 +0700

    Add %fd magic

--- kora/install/fd.py
+++ kora/install/fd.py
@@ -1,5 +1,10 @@
 import os
+from IPython.core.magic import register_line_magic
 
 os.system('wget -qO fd.deb https://github.com/sharkdp/fd/releases/download/v8.1.1/fd_8.1.1_amd64.deb')
 os.system('dpkg -i fd.deb')
 os.remove('fd.deb')
+
+@register_line_magic
+def fd(line):
+    get_ipython().system('fd '+line)
