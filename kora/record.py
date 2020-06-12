ec44d49d34771c883d9e980f805f22138898e8a6
Korakot Chaovavanich <korakot@gmail.com>
2020-06-13 01:02:00 +0700

    Add record audio

---
+++ kora/record.py
@@ -0,0 +1,44 @@
+from IPython.display import Javascript, Audio
+from google.colab import output
+from base64 import b64decode
+
+
+RECORD_AUDIO = """
+const sleep  = time => new Promise(resolve => setTimeout(resolve, time))
+const b2text = blob => new Promise(resolve => {
+    const reader = new FileReader()
+    reader.onloadend = e => resolve(e.srcElement.result)
+    reader.readAsDataURL(blob)
+})
+var record = time => new Promise(async resolve => {
+    stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+    recorder = new MediaRecorder(stream)
+    chunks = []
+    recorder.ondataavailable = e => chunks.push(e.data)
+    recorder.start()
+    await sleep(time)
+    recorder.onstop = async ()=>{
+        blob = new Blob(chunks)
+        text = await b2text(blob)
+        resolve(text)
+    }
+    recorder.stop()
+})
+"""
+
+def audio(sec=3):
+    """ Record audio for a given duration """
+    display(Javascript(RECORD_AUDIO))
+    s = output.eval_js('record(%d)' % (sec*1000))
+    b = b64decode(s.split(',')[1])
+    return Audio(b)
+
+
+# Add to class Audio
+def _audio_save(self, filename=None):
+    """ Audio's method to save as a file """
+    if filename is None:
+        filename = 'audio.wav'  # default name
+    with open(filename, 'wb') as f:
+        f.write(self.data)
+Audio.save = _audio_save
