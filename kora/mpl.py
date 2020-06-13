0689db134edd60e61c720424e800e545509f4925
Korakot Chaovavanich <korakot@gmail.com>
2020-06-13 23:37:30 +0700

    Add default font size=18

--- kora/mpl.py
+++ kora/mpl.py
@@ -5,6 +5,7 @@ from matplotlib import *  # font_manager, ft2font, rc
 def add_thai_font():
     url = 'https://raw.githubusercontent.com/korakot/kora/40a2bb269383501965c0745055f1ad4f23e167a3/THSarabunChula.ttf'
     add_font(url)
+    set_font(size=18)
 
 add_font_thai = add_thai_font
 
