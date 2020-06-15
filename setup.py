6307781ff860d3b9dc72c2b1d1167ea9db2e59e3
Korakot Chaovavanich <korakot@gmail.com>
2020-06-15 20:28:50 +0700

    0.1.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kora", 
-    version="0.1.9",
+    version="0.1.10",
     author="Korakot Chaovavanich",
     author_email="korakot@gmail.com",
     description="Convenient tools for Colab",
