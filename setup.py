d69d819d46468098394ecb6231120c004d103302
Korakot Chaovavanich <korakot@gmail.com>
2020-06-16 16:17:56 +0700

    Add xattr.get_id(fname)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kora", 
-    version="0.1.12",
+    version="0.1.13",
     author="Korakot Chaovavanich",
     author_email="korakot@gmail.com",
     description="Convenient tools for Colab",
