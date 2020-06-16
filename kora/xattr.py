74df1c26d8b46c5e1b49c33b851869d9c12f747a
Korakot Chaovavanich <korakot@gmail.com>
2020-06-16 16:20:44 +0700

    Add import *

--- kora/xattr.py
+++ kora/xattr.py
@@ -1,9 +1,9 @@
 import os
 os.system('apt install xattr')
-import xattr
+from xattr import *
 
 def get_id(filename):
     """ Google Drive FileID """
-    x = xattr.xattr(filename)
+    x = xattr(filename)
     file_id = x.get('user.drive.id')
     return file_id.decode()  # b'' -> str
