e7e1a7c69687415fa40260dc563235ce2ef49417
Korakot Chaovavanich <korakot@gmail.com>
2020-06-17 16:34:46 +0700

    kora.s for keras

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kora", 
-    version="0.1.15",
+    version="0.2.0",
     author="Korakot Chaovavanich",
     author_email="korakot@gmail.com",
     description="Convenient tools for Colab",
