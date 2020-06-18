8d0333027c46a95d7aceff0d38e1f3d12cd196d2
Korakot Chaovavanich <korakot@gmail.com>
2020-06-18 23:53:44 +0700

    Add plotnine + thai font

---
+++ plotnine.py
@@ -0,0 +1,5 @@
+from kora import mpl
+
+mpl.add_font('https://github.com/Phonbopit/sarabun-webfont/raw/master/fonts/thsarabunnew-webfont.ttf')
+from plotnine import *
+theme_set(theme_minimal(14, 'TH Sarabun New'))
