aa35c2a7e4dd249f362b68d46061076833fe9aea
Korakot Chaovavanich <korakot@gmail.com>
2020-06-19 09:10:16 +0700

    Add kora.trimesh

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kora", 
-    version="0.2.9",
+    version="0.2.10",
     author="Korakot Chaovavanich",
     author_email="korakot@gmail.com",
     description="Convenient tools for Colab",
