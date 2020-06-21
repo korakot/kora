2d3e5d36f09b4e2287d45e5ab85f0f64e52d7cd9
Korakot Chaovavanich <korakot@gmail.com>
2020-06-21 21:58:14 +0700

    kora.reload.off()

--- kora/reload.py
+++ kora/reload.py
@@ -1,9 +1,16 @@
 """ make calling autoreload easier """
 
-from importlib import reload    # -> from kora.autoreload import reload
+# from importlib import reload    # probably don't need manual reload
 from IPython import get_ipython
 
 
 magic = get_ipython().run_line_magic
 magic('load_ext', 'autoreload')
-magic('autoreload', '2')
+magic('autoreload', '2')  # on by default
+
+
+def on():
+    magic('autoreload', '2')
+
+def off():
+    magic('autoreload', '0')
