7f75341375a7ac8c3e2801aabf4e755833707f4c
Korakot Chaovavanich <korakot@gmail.com>
2020-07-01 17:20:19 +0700

    move to vistec-ai/kora

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kora", 
-    version="0.3.4",
+    version="0.3.5",
     author="Korakot Chaovavanich",
     author_email="korakot@gmail.com",
     description="Convenient tools for Colab",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/korakot/kora",
+    url="https://github.com/vistec-ai/kora",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
