cae0f51b0a344b96565e1e496f85786e6ce77bf7
Korakot Chaovavanich <korakot@gmail.com>
2020-07-03 00:37:22 +0700

    selenium __str__ and __repr__

--- kora/selenium.py
+++ kora/selenium.py
@@ -10,5 +10,10 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 # create a webdriver instance, ready to use
 wd = webdriver.Chrome('chromedriver',options=options)
+
+# make it easier to query and explore elements
 wd.select = wd.find_elements_by_css_selector
 wd.select1 = wd.find_element_by_css_selector
+from selenium.webdriver.remote.webelement import WebElement
+WebElement.__str__ = lambda self: self.get_attribute('outerHTML')
+WebElement.__repr__ = WebElement.__str__
