311b0b39d00b4816f794626552eb92ef6f94e6d4
Korakot Chaovavanich <korakot@MBP-kora.local>
2020-07-08 11:46:07 +0700

    Add get_ver(pkg)

--- kora/__init__.py
+++ kora/__init__.py
@@ -1,3 +1,6 @@
 from pkg_resources import get_distribution
 
-__version__ = get_distribution('kora').version
+def get_ver(package):
+    return get_distribution(package).version
+
+__version__ = get_ver('kora')
