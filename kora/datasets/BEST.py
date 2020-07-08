a9317ba96b1b4a06b7ce5430ee6ac52b6c42e541
Korakot Chaovavanich <korakot@MBP-kora.local>
2020-07-08 15:44:49 +0700

    kora.datasets to help download

---
+++ kora/datasets/BEST.py
@@ -0,0 +1,20 @@
+"""
+Best dataset
+
+For thai word segmentation
+"""
+import os
+from urllib.request import urlretrieve
+
+
+def download():
+    base = 'http://www.nectec.or.th/corpuso/phocadownload/dl_text_thai-eng/BEST/TrainingSet/'
+    genres = ['article', 'encyclopedia', 'news', 'novel']
+    print("Start downloading: .. ")
+    for genre in genres:
+        url = base + genre + '.zip'
+        fname, _ = urlretrieve(url)
+        os.system(f'unzip {fname}')
+        print(genre, end=' .. ')
+    print('\nThe results are in these 4 folders')
+    return genres
---
+++ kora/datasets/README.md
@@ -0,0 +1,9 @@
+# kora.datasets
+
+Download dataset easily.
+
+Some data will be preprocessed to make it more ready to use.
+
+Currently
+
+- BEST: Thai word segmentation
