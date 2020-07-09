15a4e55fda602d611c09d7e4fb03eb0c3afdb01e
Korakot Chaovavanich <korakot@MBP-kora.local>
2020-07-09 16:07:19 +0700

    Avoide message

--- kora/datasets/__init__.py
+++ kora/datasets/__init__.py
@@ -1,1 +1,10 @@
+import os
+import sys
+stderr = sys.stderr
+sys.stderr = open(os.devnull, 'w')  # avoid printing "Using TensorFlow backend."
+
 from keras.datasets import *
+
+sys.stderr = stderr
+
+
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kora", 
-    version="0.4.9",
+    version="0.4.10",
     author="Korakot Chaovavanich",
     author_email="korakot@gmail.com",
     description="Convenient tools for Colab",
