3183f977f783c8367ca150ed94edc3e82090b762
Korakot Chaovavanich <korakot@gmail.com>
2020-09-19 23:27:21 +0700

    Allow importing file.ipynb

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kora", 
-    version="0.6.12",
+    version="0.6.14",
     author="Korakot Chaovavanich",
     author_email="korakot@gmail.com",
     description="Convenient tools for Colab",
