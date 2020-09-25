6ab50a59c3156ec0447d111d0d1ebe6b3cca836a
Korakot Chaovavanich <korakot@gmail.com>
2020-09-25 07:59:38 +0700

    Add install.pg13

---
+++ kora/install/pg13.py
@@ -0,0 +1,25 @@
+import os
+
+# update apt
+os.system("curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -")
+with open('/etc/apt/sources.list.d/pgdg.list', 'w') as f:
+  f.write("deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main")
+os.system("apt update")
+
+# install PostgreSQL 13
+os.system("apt install postgresql-13 postgresql-client-13")
+os.system("service postgresql start")
+os.system("sudo -u postgres psql -c 'CREATE USER root WITH SUPERUSER'")
+
+# update %%sql and add pg special commands
+os.system('pip install -U ipython-sql')
+os.system('pip install pgspecial')
+os.system('pip install psycopg2-binary')  # avoid warning
+
+# config for %%sql
+magic = get_ipython().run_line_magic
+magic('load_ext', 'sql')
+magic('config', 'SqlMagic.displaycon=False')
+magic('config', 'SqlMagic.feedback=False')
+magic('config', 'SqlMagic.autopandas=True')
+magic('sql', 'postgresql+psycopg2://@/postgres')
