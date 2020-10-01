efc099a44af4be2033a122d250e8b26e8124d75c
Korakot Chaovavanich <korakot@MBP-kora.local>
2020-10-01 15:23:54 +0700

    Add histogram

---
+++ kora/viz/__init__.py
@@ -0,0 +1,15 @@
+"""
+__init__.py contains only small viz I use frequently
+"""
+import pandas as pd
+from collections import Counter
+
+def histogram(data, name='num', **kw):
+    """
+    Doesn't do binning, just a bar chart of Counter()
+    **kw can be title, width, or range_x=(0,10)
+    """
+    import plotly.express as px
+    items = Counter(data).items()
+    df = pd.DataFrame(items, columns=[name, 'freq'])
+    return px.bar(df, x=name, y='freq', **kw)
