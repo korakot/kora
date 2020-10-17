aebe7fc0f8f0448540bd6d24c0b0b62db3b3bcae
Korakot Chaovavanich <korakot@gmail.com>
2020-10-17 18:41:23 +0700

    Add kaldi/src back

--- kora/install/kaldi.py
+++ kora/install/kaldi.py
@@ -6,5 +6,14 @@ import os
 # Just extract from my custom tar. Very fast (17s)
 url = 'https://github.com/airesearch-in-th/kora/releases/download/v0.7/kaldi.tar.gz'
 os.system(f"curl -L {url} | tar xz -C /")
-# Add openfst to path
-os.environ['PATH'] += ':/opt/kaldi/tools/openfst/bin'
+
+# Add paths to binaries
+paths = [''] + """
+/opt/kaldi/src/bin
+/opt/kaldi/src/featbin
+/opt/kaldi/src/fstbin
+/opt/kaldi/src/gmmbin
+/opt/kaldi/tools/openfst/bin
+""".split()
+
+os.environ['PATH'] += ':'.join(paths)
