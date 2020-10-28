72d25aed24d715eb6bd6bbc5547afc76bee936fd
Korakot Chaovavanich <korakot@MBP-kora.local>
2020-10-28 09:34:40 +0700

    Add TSync2 corpus

---
+++ kora/datasets/TSync2.py
@@ -0,0 +1,15 @@
+"""
+TSync2 Speech Corpus for TTS, by NECTEC
+Licensed under CC-BY-NC-SA
+"""
+
+import os
+
+
+def download():
+    url = "https://github.com/korakot/corpus/releases/download/v1.0/AIFORTHAI-TSync2Corpus.zip"
+    print("NECTEC licenses TSync2 under CC-BY-NC-SA")
+    print("Start downloading: .. ")
+    os.system(f"wget {url}")
+    os.system("unzip AIFORTHAI-TSync2Corpus.zip")
+    print("Finished")
