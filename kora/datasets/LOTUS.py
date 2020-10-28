91b02eb621802abe0418df9b9ee7ff2f76762a14
Korakot Chaovavanich <korakot@MBP-kora.local>
2020-10-28 09:23:12 +0700

    Lotus speech corpus

---
+++ kora/datasets/LOTUS.py
@@ -0,0 +1,16 @@
+"""
+LOTUS Speech Corpus for ASR, by NECTEC
+Licensed under CC-BY-NC-SA
+"""
+
+import os
+
+
+def download():
+    url = "https://github.com/korakot/corpus/releases/download/v1.0/AIFORTHAI-LotusCorpus.zip"
+    print("NECTEC licenses LOTUS under CC-BY-NC-SA")
+    print("Start downloading: .. ")
+    os.system(f"wget {url}")
+    os.system("unzip AIFORTHAI-LotusCorpus.zip")
+    os.system("mv LOTUS-CD-FREE LOTUS")  # rename
+    print("Finished")
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
 
 setuptools.setup(
     name="kora", 
-    version="0.8.15",
+    version="0.8.17",
     author="Korakot Chaovavanich",
     author_email="korakot@gmail.com",
     description="Convenient tools for Colab",
