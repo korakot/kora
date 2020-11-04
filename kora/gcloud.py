ae8e74204cdab871e3f458ecc1daf4c18bc21cf3
Korakot Chaovavanich <korakot@MBP-kora.local>
2020-11-04 16:07:49 +0700

    Use ipython's shell

--- kora/gcloud.py
+++ kora/gcloud.py
@@ -3,11 +3,13 @@ Convenient tools that call gcloud or gsutil
 or work with Google Cloud Platform
 """
 import os
+from IPython import get_ipython
 
+shell = get_ipython().system
 
 def config(project='kora-id'):
     os.system(f"gcloud config set project {project}")
 
 def login():
-    os.system('gcloud auth login')
+    shell('gcloud auth login')
 
