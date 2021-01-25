8723789ad88ace17db1bc46fcee30efd4d649e44
Korakot Chaovavanich <korakot@gmail.com>
2021-01-25 22:49:17 +0700

    Update to new dataprep.eda

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
 
 setuptools.setup(
     name="kora", 
-    version="0.9.17",
+    version="0.9.18",
     author="Korakot Chaovavanich",
     author_email="korakot@gmail.com",
     description="Convenient tools for Colab",
